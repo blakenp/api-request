37a13b8580d3d596ce3ae1030c616793ed6774f7
Blake Peterson <blake.ptrsn18@gmail.com>
2023-05-19 12:39:24 -0600

    Add API Request to get data from webhooks website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,32 @@
 'use client';
 
 import React from 'react';
-import { redirect } from "next/navigation";
-import axios from 'axios';
+import { useState } from 'react';
 
-export default function Redirect() {
-  const getRedirect = async() => {
-    redirect(`https://webhooks-black.vercel.app/api/webhook`);
+export default function Home() {
+  const [comments, setComments] = useState<any[]>([])
+
+  const getComments = async () => {
+    await fetch('https://webhooks-black.vercel.app/api/webhook')
+    .then(async (response) => {
+      let data = await response.json()
+      console.log(data[0].text)
+      setComments(data)
+    })
   }
 
   return (
-    <div>
-      <button onClick={getRedirect}>Send Webhook Request</button>
-    </div>
-  );
-};
+    <>
+      <button onClick={getComments}>Load Comments</button>
+      {
+        comments.map(comment => {
+          return (
+            <div key={comment.id}>
+              {comment.id} {comment.text}
+            </div>
+          )
+        })
+      }
+    </>
+  )
+}
