8233880fe94b331bd37b867e72394c6930040958
Blake Peterson <blake.ptrsn18@gmail.com>
2023-05-20 09:43:39 -0600

    Add POST request function to submit new comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,10 @@ import { useState } from 'react';
 
 export default function Home() {
   const [comments, setComments] = useState<any[]>([])
+  const [comment, setComment] = useState("")
 
   const getComments = async () => {
-    await fetch('https://webhooks-black.vercel.app/api/webhook')
+    await fetch('/api/webhook')
     .then(async (response) => {
       let data = await response.json()
       console.log(data[0].text)
@@ -15,8 +16,25 @@ export default function Home() {
     })
   }
 
+  const submitComment = async () => {
+    await fetch('/api/webhook', {
+      method: 'POST',
+      body: JSON.stringify({ comment: comment }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    setComment('')
+  }
+
   return (
     <>
+      <input 
+        type='text'
+        value={comment}
+        onChange={(event) => setComment(event.target.value)}
+      />
+      <button onClick={submitComment}>Submit comment</button>
       <button onClick={getComments}>Load Comments</button>
       {
         comments.map(comment => {
